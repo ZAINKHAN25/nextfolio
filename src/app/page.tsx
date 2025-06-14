a2e6b3aa46c3f9cdaa0564a058b1246bf3422476
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2025-06-14 13:14:32 +0500

    Work in Progress.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
-'use client';
+"use client";
 
-import Hero from '@/components/sections/hero';
-import About from '@/components/sections/about';
-import Experience from '@/components/sections/experience';
-import Projects from '@/components/sections/projects';
-import Services from '@/components/sections/services';
-import Contact from '@/components/sections/contact';
-import Header from '@/components/header';
-import Footer from '@/components/footer';
-import { useEffect } from 'react';
-import { initGA } from '@/lib/analytics';
+import Hero from "@/components/sections/hero";
+import About from "@/components/sections/about";
+import Experience from "@/components/sections/experience";
+import Projects from "@/components/sections/projects";
+import Services from "@/components/sections/services";
+import Contact from "@/components/sections/contact";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import AiChatBot from "@/components/AiChatBot";
 
+import { useEffect } from "react";
+import { initGA } from "@/lib/analytics";
 
 export default function Home() {
   useEffect(() => {
@@ -23,6 +24,7 @@ export default function Home() {
     <main>
       <div className="min-h-screen bg-white dark:bg-dark-200 transition-colors duration-300">
         <Header />
+        <AiChatBot />
         <Hero />
         <About />
         <Experience />
---
+++ src/components/AiChatBot.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function AiChatBot() {
+  return (
+    <div>
+        
+      <iframe
+        src="https://www.chatbase.co/chatbot-iframe/6UmUbM3fgnxNEGifyp2vX"
+        width="100%"
+        style={{ height: "100%", minHeight: "700px" }}
+        frameBorder="0"
+      ></iframe>
+    </div>
+  );
+}
+
+export default AiChatBot;
